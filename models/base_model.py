3e516d854dac339e8f7856909be5e42edebae83d
Walton Lee <mr.waltonlee@me.com>
2017-03-22 04:06:30 +0000

    updating BaseModel to prepare for SQLAlchemy

--- models/base_model.py
+++ models/base_model.py
@@ -1,25 +1,42 @@
 #!/usr/bin/python3
-import datetime
+from datetime import datetime
 import uuid
 import models
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
 
 
 class BaseModel:
     """The base class for all storage objects in this project"""
+    id = Column(String(60), unique=True, nullable=False, pimary_key=True)
+    created_at = Column(DateTime(), nullable=False, default=datetime.now())
+    updated_at = Column(Datetime(), nullable=False, default=datetime.now())
+
+
     def __init__(self, *args, **kwargs):
         """initialize class object"""
         if len(args) > 0:
             for k in args[0]:
                 setattr(self, k, args[0][k])
         else:
-            self.created_at = datetime.datetime.now()
+            self.created_at = datetime.now()
             self.id = str(uuid.uuid4())
+
+        if kwargs is not None:
+            for k in kwargs[0]:
+                setattr(self, k, kwargs[0][k])
+
+        """
         for k in kwargs:
             print("kwargs: {}: {}".format(k, kwargs[k]))
+        """
 
     def save(self):
         """method to update self"""
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
         models.storage.new(self)
         models.storage.save()
 
@@ -32,7 +49,13 @@ class BaseModel:
         """convert to json"""
         dupe = self.__dict__.copy()
         dupe["created_at"] = str(dupe["created_at"])
+        if ("_sa_instance_state" in dupe):
+            dupe.pop('_sa_instance_state', None)
         if ("updated_at" in dupe):
             dupe["updated_at"] = str(dupe["updated_at"])
         dupe["__class__"] = type(self).__name__
         return dupe
+
+    def delete(self):
+        """deleting current instance"""
+        models.storage.delete(self.id)

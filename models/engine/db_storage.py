88207de3683e9235847b528e772985aaaa5acb39
Walton Lee <mr.waltonlee@me.com>
2017-03-23 07:41:15 +0000

    updating

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -6,7 +6,7 @@ manages database storage
 from sqlalchemy import create_engine
 from os import getenv
 from sqlalchemy.orm import sessionmaker
-from models.base_model import BaseModel, Base
+from models.base_model import Base
 from models.user import User
 from models.state import State
 from models.city import City
@@ -21,27 +21,26 @@ class DBStorage:
     __engine = None
     __session = None
 
-    def init(self):
-        self.__engine = create_engine('mysql+mymsqldb://{}:{}@{}/{}',
-                                      getenv("HBNB_MYSQL_USER"),
-                                      getenv("HBNB_MYSQL_PWD"),
-                                      getenv("HBNB_MYSQL_HOST"),
-                                      getenv("HBNB_MYSQL_DB") )
-
-        if getenv("HBNB_MYSQL_ENV") == "test":
+    def __init__(self):
+        self.__engine = create_engine('mysql+mymsqldb://{}:{}@{}/{}'.format(
+                                      getenv('HBNB_MYSQL_USER'),
+                                      getenv('HBNB_MYSQL_PWD'),
+                                      getenv('HBNB_MYSQL_HOST'),
+                                      getenv('HBNB_MYSQL_DB')))
+        self.__my_list = {"User":User}
+        if getenv('HBNB_MYSQL_ENV') == "test":
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
         new_dict={}
-        my_list = ["User", "State", "City", "Amenity", "Place", "Review"]
+#        my_list = ["User", "State", "City", "Amenity", "Place", "Review"]
         if cls is None:
             for item in self.__session.query(my_list):
                 new_dict[item.id] = item
-            return (new_dict)
         else:
             for item in self.__session.query(cls):
                 new_dict[item.id] = item
-            return (new_dict)
+        return (new_dict)
 
     def new(self, obj):
         self.__session.add(obj)

22adaae6e3d6ce328feea0b93cc455c02998dd98
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-03-22 17:15:31 -0700

    Updating base_model.py and place.py for sqlalchemy

--- models/place.py
+++ models/place.py
@@ -1,18 +1,29 @@
 #!/usr/bin/python3
+"""
+Place Class:
+    inherits from base
+"""
 from models import *
 
 
-class Place(BaseModel):
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+class PlaceAmenity(Base):
+    __tablename__ = "place_amenity"
+    place_id = Column(String(60), nullable=False,
+                      ForeignKey("places.id"), primary_key=True)
+    amenity_id = Column(String(60), nullable=False, ForeignKey("amenities.id"), primary_key=True)
+
+class Place(BaseModel, Base):
+    __tablename__ = "places"
+    city_id = Column(String(60), nullable=False, ForeignKey("cities.id"))
+    user_id = Column(String(60), nullable=False, ForeignKey("users.id"))
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=False)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float, nullable=False)
+    longitude = Column(Float, nullable=False)
     amenities = [""]
 
     def __init__(self, *args, **kwargs):

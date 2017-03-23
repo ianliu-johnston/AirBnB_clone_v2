cc245daf2a9fc49be93c6fde23dbde923d9713ff
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-03-22 20:27:15 -0700

    Fixed syntax

--- models/setup_mysql_dev.sql
+++ models/setup_mysql_dev.sql
@@ -1,5 +1,5 @@
---Sets up a MySQL db
+-- Sets up a MySQL db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT SELECT ON 'performance_schema'.* TO 'hbnb_dev'@'localhost'
+GRANT SELECT PRIVILEGES ON 'performance_schema'.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;

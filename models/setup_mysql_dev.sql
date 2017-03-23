99d077bdb43265307190dd708f3f481e53d63785
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-03-22 20:26:03 -0700

    Task 2 + 3: Checking if works

---
+++ models/setup_mysql_dev.sql
@@ -0,0 +1,5 @@
+--Sets up a MySQL db
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT ON 'performance_schema'.* TO 'hbnb_dev'@'localhost'
+FLUSH PRIVILEGES;
---
+++ models/setup_mysql_test.sql
@@ -0,0 +1,5 @@
+--Sets up a MySQL db
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT SELECT ON 'performance_schema'.* TO 'hbnb_test'@'localhost'
+FLUSH PRIVILEGES;

63a126c19792d6643f05b26cae3c1ebc7c3cf034
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-03-23 22:12:41 -0700

    moved imports to inside of conditionals

--- models/__init__.py
+++ models/__init__.py
@@ -5,13 +5,13 @@ from models.amenity import Amenity
 from models.city import City
 from models.place import Place
 from models.review import Review
-from models.engine import db_storage
-from models.engine import file_storage
 from os import getenv
 
 
 if getenv("HBNB_TYPE_STORAGE") == "db":
+    from models.engine import db_storage
     storage = db_storage.DBStorage()
 else:
+    from models.engine import file_storage
     storage = file_storage.FileStorage()
 storage.reload()

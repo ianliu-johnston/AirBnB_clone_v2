3d778ffe1bb49a9b1ce228755bea1e4201658d04
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-03-28 10:29:36 -0700

    Task 0: Fixed for style and added documentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 from flask import Flask
+"""
+Very Simple Flask hello world
+"""
+
 
 app = Flask(__name__)
+
+
 @app.route('/')
 def hello_HBNB():
+
+    """
+    Just returns text
+    """
     return (' Hello HBNB!')

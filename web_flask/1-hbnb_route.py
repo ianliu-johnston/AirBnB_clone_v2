61816ab26432d260a3cd8f247d547864d9fdf7cd
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-03-28 10:33:55 -0700

    Task 1: Complete

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from flask import Flask
+"""
+Very Simple Flask hello world
+"""
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_HBNB():
+
+    """
+    Just returns text
+    """
+    return ('Hello HBNB!\n')
+
+
+@app.route('/hbnb')
+def HBNB():
+
+    """
+    Returns the route /hbnb
+    """
+    return ('HBNB\n')

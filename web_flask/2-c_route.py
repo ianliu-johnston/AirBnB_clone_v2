c51831d8418ef3679cb7333694a9682863c6e897
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-03-28 10:39:17 -0700

    Task 2: Complete

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+from flask import Flask
+"""
+Very Simple Flask hello world
+"""
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_HBNB():
+
+    """
+    Just returns text
+    """
+    return ('Hello HBNB!\n')
+
+
+@app.route('/hbnb')
+def HBNB():
+
+    """
+    Returns the route /hbnb
+    """
+    return ('HBNB\n')
+
+
+@app.route('/c/<value>')
+def c_is(value):
+
+    """
+    Returns the route /c/<value>
+    where <value> is any URI that the user requests
+    """
+    return ("C {:s}\n".format(value))
